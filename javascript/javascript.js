9432b59cadfeab38ff3ca33b1ee0b28947e8bf8c
Robin Hooda <robinhooda66@gmail.com>
2019-09-06 19:33:19 +0530

    month and date added into functionality in the home screen

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -33,6 +33,8 @@
 let currentDate = new Date();
 let minute= currentDate.getMinutes();
 let dayInNumber= currentDate.getDay();
+var monthInNumber=currentDate.getMonth()-1;
+var dateInNumber=currentDate.getDate();
 let dayInString = dayInNumber==0?
         'Sunday':dayInNumber==1?
         'Monday':dayInNumber==2?
@@ -40,13 +42,27 @@ let dayInString = dayInNumber==0?
         'Wednesday':dayInNumber==4?
         'Thursday':dayInNumber==5?
         'Friday':'Saturday';
+    let monthInString = monthInNumber==0?
+        'Jan':monthInNumber==1?
+        'Feb':monthInNumber==2?
+        'March':monthInNumber==3?
+        'April':monthInNumber==4?
+        'June':monthInNumber==5?
+        'July':monthInNumber==6?
+        'Aug':monthInNumber==7?
+        'Sept':monthInNumber==8?
+        'Oct':monthInNumber==9?
+        'Nov':'Dec';
     document.getElementById("hour").innerHTML=currentDate.getHours();
     if(minute<10)
-    {
-        document.getElementById("mins").innerHTML="0"+currentDate.getMinutes();
-    }else
-        document.getElementById("mins").innerHTML=currentDate.getMinutes();
-    document.getElementById("day").innerHTML=dayInString;
+            document.getElementById("mins").innerHTML="0"+currentDate.getMinutes();
+    else
+            document.getElementById("mins").innerHTML=currentDate.getMinutes();
+    document.getElementById("day").innerHTML=dayInString;   
+    if(dateInNumber<10)
+            document.getElementById("month").innerHTML=monthInString+"- 0"+dateInNumber;
+    else
+            document.getElementById("month").innerHTML=monthInString+"-"+dateInNumber;
     
     
 
